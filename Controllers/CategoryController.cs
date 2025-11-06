1f43b3fadb54c3099e4c37e650c1c9b9a6c5be31
Ali Umur Kucur <aliumurkucur@gmail.com>
2025-11-06 09:21:03 +0300

    CategoryController created.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniCrmApi.Models;
+using MiniCrmApi.Services;
+using System.Threading.Tasks;
+
+namespace MiniCrmApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            List<Category> categoryList = await categoryService.GetAllCategoriesAsync();
+
+            return Ok(categoryList);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(int id)
+        {
+            Category category = await categoryService.GetCategoryByIdAsync(id);
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory([FromBody] Category category)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await categoryService.AddCategoryAsync(category);
+
+            return CreatedAtAction(nameof(GetCategoryById), new { Id = category.Id }, category);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, [FromBody] Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await categoryService.UpdateCategoryAsync(id, category);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            await categoryService.DeleteCategoryAsync(id);
+            return NoContent();
+        }
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,13 @@
-﻿namespace MiniCrmApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniCrmApi.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Category name is required.")]
+        [StringLength(50,MinimumLength = 10, ErrorMessage = "Category name length should be between 10 to 50.")]
         public string Name { get; set; } = string.Empty;
 
         //Navigation Property
