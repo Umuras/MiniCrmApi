c9b14fd4678769b6c749bf4aef1b7d5de2ed4b52
Ali Umur Kucur <aliumurkucur@gmail.com>
2025-11-06 11:31:39 +0300

    ProductController created.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniCrmApi.Models;
+using MiniCrmApi.Services;
+
+namespace MiniCrmApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            List<Product> productList = await productService.GetAllProductsAsync();
+            return Ok(productList);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            Product product = await productService.GetProductByIdAsync(id);
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await productService.AddProductAsync(product);
+
+            return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, [FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await productService.UpdateProductAsync(id, product);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            await productService.DeleteProductAsync(id);
+            return NoContent();
+        }
+    }
+}
